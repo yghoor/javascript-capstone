a90d3aeb1dc339e1a2576dd041d6189dd6eb2b07
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-28 20:33:18 +0200

    Import icon files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import logoIcon from './images/icons/logo-icon.png';
+import breakfastIcon from './images/icons/breakfast-icon.png';
+import vegetarianIcon from './images/icons/vegetarian-icon.png';
+import dessertIcon from './images/icons/dessert-icon.png';
+import heartEmptyIcon from './images/icons/heart-empty-icon.png';
+import heartFilledIcon from './images/icons/heart-filled-icon.png';
