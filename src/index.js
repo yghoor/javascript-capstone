8d9276066041ff3e85faab0be9658ff1bbb429a9
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-28 20:41:46 +0200

    Update imports for icons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css';
-import logoIcon from './images/icons/logo-icon.png';
-import breakfastIcon from './images/icons/breakfast-icon.png';
-import vegetarianIcon from './images/icons/vegetarian-icon.png';
-import dessertIcon from './images/icons/dessert-icon.png';
-import heartEmptyIcon from './images/icons/heart-empty-icon.png';
-import heartFilledIcon from './images/icons/heart-filled-icon.png';
+import logoIcon from './logo-icon.png';
+import breakfastIcon from './breakfast-icon.png';
+import vegetarianIcon from './vegetarian-icon.png';
+import dessertIcon from './dessert-icon.png';
+import heartEmptyIcon from './heart-empty-icon.png';
+import heartFilledIcon from './heart-filled-icon.png';
 
 // Select icon placeholders on page
 
