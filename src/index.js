768c577d70c7ccb14a33739af78141aefdf33c22
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-28 20:34:29 +0200

    Display icons on page load

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import dessertIcon from './images/icons/dessert-icon.png';
 import heartEmptyIcon from './images/icons/heart-empty-icon.png';
 import heartFilledIcon from './images/icons/heart-filled-icon.png';
 
+// Select icon placeholders on page
+
 const logoImg = document.getElementById('logo-icon');
 const breakfastImg = document.getElementById('breakfast-icon');
 const vegetarianImg = document.getElementById('vegetarian-icon');
@@ -13,3 +15,15 @@ const dessertImg = document.getElementById('dessert-icon');
 const heartEmptyImgs = document.querySelectorAll('.heart-empty-icon');
 const heartFilledImgs = document.querySelectorAll('.heart-filled-icon');
 
+// Display icons
+
+logoImg.src = logoIcon;
+breakfastImg.src = breakfastIcon;
+vegetarianImg.src = vegetarianIcon;
+dessertImg.src = dessertIcon;
+heartEmptyImgs.forEach((img) => {
+  img.src = heartEmptyIcon;
+});
+heartFilledImgs.forEach((img) => {
+  img.src = heartFilledIcon;
+});
