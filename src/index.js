2c38d4c0367de7db5613232f6b41a027fb37e184
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-28 20:33:36 +0200

    Select icon placeholders on page

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import vegetarianIcon from './images/icons/vegetarian-icon.png';
 import dessertIcon from './images/icons/dessert-icon.png';
 import heartEmptyIcon from './images/icons/heart-empty-icon.png';
 import heartFilledIcon from './images/icons/heart-filled-icon.png';
+
+const logoImg = document.getElementById('logo-icon');
+const breakfastImg = document.getElementById('breakfast-icon');
+const vegetarianImg = document.getElementById('vegetarian-icon');
+const dessertImg = document.getElementById('dessert-icon');
+const heartEmptyImgs = document.querySelectorAll('.heart-empty-icon');
+const heartFilledImgs = document.querySelectorAll('.heart-filled-icon');
+
