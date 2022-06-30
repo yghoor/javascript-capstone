bbd3d4ed4c19bc1f7e641c817e4c38925d2af06f
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-30 18:46:24 +0200

    Build function to call addModal function
    With recipe data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import { addModal, createRecipePage } from './createPage.js';
 
 let breakfastMeals;
 
+async function fetchRecipeAndaddModal(recipeId) {
+  let recipeData;
+  await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`)
+    .then((response) => response.json())
+    .then((result) => {
+      // eslint-disable-next-line prefer-destructuring
+      recipeData = result.meals[0];
+    });
+
+  addModal(recipeData);
+}
+
 async function fetchRecipesAndCreatePage() {
   await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast')
     .then((response) => response.json())
