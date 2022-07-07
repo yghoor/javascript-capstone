3571d21744624b7e31c5096149a83c9b220b3ac7
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-07 12:14:53 +0200

    Build async function to display like count
    Inside the recipe card's 'like-count' span

--- src/add-comments-and-likes.js
+++ src/add-comments-and-likes.js
@@ -7,3 +7,18 @@ export function addHeartToggleListeners() {
   });
 }
 
+export async function displayLikeCount(recipeId, appId) {
+  let likeCounts;
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`)
+    .then((response) => response.json())
+    .then((result) => {
+      likeCounts = result;
+    });
+
+  const recipeLikeCountSpan = document.getElementById(`recipe-${recipeId}-like-count`);
+  likeCounts.forEach((likeCount) => {
+    if (likeCount.item_id === recipeId) {
+      recipeLikeCountSpan.textContent = `Likes: ${likeCount.likes} `;
+    }
+  });
+}
