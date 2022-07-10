5e5b1a0897812651b05898d9615898690f35c087
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-10 22:43:11 +0200

    Create funcion to display recipe count

--- src/counters.js
+++ src/counters.js
@@ -2,3 +2,8 @@ export function countRecipes(mealsArray) {
   return mealsArray.length;
 }
 
+export function displayRecipeCount(mealsArray, category) {
+  const navLink = document.getElementById(`${category.toLowerCase()}-meals-count`);
+  navLink.innerHTML = `(${countRecipes(mealsArray)})`;
+}
+
